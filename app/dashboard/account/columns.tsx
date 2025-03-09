4fdb2f6de8186600d7c8882b71eaebcf07f0d297
Do Quang Trieu <quangtrieu2703@gmail.com>
2025-03-09 21:16:06 +0700

    added resuseable component for table

--- app/dashboard/account/columns.tsx
+++ app/dashboard/account/columns.tsx
@@ -2,7 +2,7 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { Learner } from "@/types/learner";
-import { MoreHorizontal } from "lucide-react";
+import { MoreHorizontal, ArrowUpDown, Activity } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -12,11 +12,36 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Checkbox } from "@/components/ui/checkbox";
+import { DataTableColumnHeader } from "@/components/framework/column-header";
 
 export const columns: ColumnDef<Learner>[] = [
   {
-    header: () => <div className="text-left">Name</div>,
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+  },
+  {
+    header: ({ column }) => {
+      return <DataTableColumnHeader title="Name" column={column} />;
+    },
     accessorKey: "name",
+    cell: ({ row }) => <div className="text-left">{row.original.name}</div>,
     // cell: ({ row }) => (
     //   <div className="text-left">
     //     <DropdownMenu>
@@ -36,27 +61,30 @@ export const columns: ColumnDef<Learner>[] = [
     // ),
   },
   {
-    header: () => <div className="text-left">Email</div>,
+    header: ({ column }) => {
+      return <DataTableColumnHeader title="Email" column={column} />;
+    },
     accessorKey: "email",
     cell: ({ row }) => <div className="text-left">{row.original.email}</div>,
   },
   {
-    header: () => <div className="text-left">Language</div>,
+    header: ({ column }) => {
+      return <DataTableColumnHeader title="Language" column={column} />;
+    },
     accessorKey: "language",
     cell: ({ row }) => <div className="text-left">{row.original.language}</div>,
   },
   {
-    header: () => <div className="text-left">Level</div>,
+    header: ({ column }) => {
+      return <DataTableColumnHeader title="Level" column={column} />;
+    },
     accessorKey: "level",
     cell: ({ row }) => <div className="text-left">{row.original.level}</div>,
   },
   {
-    header: () => <div className="text-left">Progress</div>,
-    accessorKey: "progress",
-    cell: ({ row }) => <div className="text-left">{row.original.progress}</div>,
-  },
-  {
-    header: () => <div className="text-left">Last Activity</div>,
+    header: ({ column }) => {
+      return <DataTableColumnHeader title="Last Activity" column={column} />;
+    },
     accessorKey: "lastActivity",
     cell: ({ row }) => (
       <div className="text-left">{row.original.lastActivity}</div>
--- components/framework/data-table.tsx
+++ components/framework/data-table.tsx
@@ -2,10 +2,15 @@
 
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   useReactTable,
   VisibilityState,
+  SortingState,
+  getSortedRowModel,
+  getPaginationRowModel,
 } from "@tanstack/react-table";
 import {
   Table,
@@ -23,6 +28,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { DataTablePagination } from "@/components/framework/pagination";
+import { DataTableViewOptions } from "@/components/framework/column-toggle";
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
@@ -32,98 +40,129 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+
     // getFilteredSelectedRowModel: getFilteredSelectedRowModel(),
     state: {
       columnVisibility,
+      rowSelection,
+      columnFilters,
+      sorting,
     },
   });
 
   return (
-      <div>
-        <div className="flex items-center py-4">
+    <div>
+      <div className="flex items-center py-4">
+        {/* Column Filter */}
+        <div className="flex items-center gap-2">
           <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
-              Show/Hide Columns
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter(
-                (column) => column?.getCanHide()
-              )
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column?.id}
-                    className="capitalize"
-                    checked={column?.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column?.toggleVisibility(!!value)
-                    }
-                  >
-                    {column?.id}
-                  </DropdownMenuCheckboxItem>
-                )
-              })}
-          </DropdownMenuContent>
-            </DropdownMenu>
-    </div>
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table?.getHeaderGroups()?.map((headerGroup) => (
-            <TableRow key={headerGroup?.id}>
-              {headerGroup?.headers?.map((header) => {
-                return (
-                  <TableHead key={header?.id}>
-                    {header?.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header?.column?.columnDef?.header,
-                          header?.getContext()
-                        )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table?.getRowModel()?.rows?.length ? (
-            table?.getRowModel()?.rows?.map((row) => (
-              <TableRow
-                key={row?.id}
-                data-state={row?.getIsSelected() && "selected"}
-              >
-                {row?.getVisibleCells().map((cell) => (
-                  <TableCell key={cell?.id}>
-                    {flexRender(
-                      cell?.column?.columnDef?.cell,
-                      cell?.getContext()
-                    )}
-                  </TableCell>
-                ))}
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">Filter</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table.getHeaderGroups().map((headerGroup) => (
+                <DropdownMenuCheckboxItem
+                  className="flex flex-col gap-2"
+                  key={headerGroup?.id}
+                >
+                  {headerGroup?.headers.map((header) => (
+                    <DropdownMenuCheckboxItem
+                      key={header?.id}
+                      className="capitalize"
+                    >
+                      {header?.column?.id}
+                    </DropdownMenuCheckboxItem>
+                  ))}
+                </DropdownMenuCheckboxItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <Input
+            placeholder="Filter emails..."
+            value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("email")?.setFilterValue(event.target.value)
+            }
+            className="max-w-sm"
+          />
+        </div>
+
+        {/* Show/Hide Columns */}
+        <DataTableViewOptions table={table} />
+      </div>
+
+      {/* Table */}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table?.getHeaderGroups()?.map((headerGroup) => (
+              <TableRow key={headerGroup?.id}>
+                {headerGroup?.headers?.map((header) => {
+                  return (
+                    <TableHead key={header?.id}>
+                      {header?.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header?.column?.columnDef?.header,
+                            header?.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns?.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table?.getRowModel()?.rows?.length ? (
+              table?.getRowModel()?.rows?.map((row) => (
+                <TableRow
+                  key={row?.id}
+                  data-state={row?.getIsSelected() && "selected"}
+                >
+                  {row?.getVisibleCells().map((cell) => (
+                    <TableCell key={cell?.id}>
+                      {flexRender(
+                        cell?.column?.columnDef?.cell,
+                        cell?.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns?.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </Table>
-        </div>
       </div>
+
+      {/* Pagination */}
+      <div className="py-4">
+        <DataTablePagination table={table} />
+      </div>
+    </div>
   );
 }
